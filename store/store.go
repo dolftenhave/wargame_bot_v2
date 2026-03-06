e1e6c7403c0c71803a0115b2bacb4b397c596f82
dolftenhave <dolftenhave@gmail.com>
2026-03-06 15:49:11 +1300

    Updated the store interfaces to match the changes in the domain types

--- store/store.go
+++ store/store.go
@@ -6,25 +6,41 @@ import (
 )
 
 type Store interface {
-	PlayerStore
+	WargamePlayerStore
+	DiscordPlayerStore
 	MapStore
 	ModeStore
 	IconStore
 	CommandStore
 }
 
-type PlayerStore interface {
-	NewPlayer(ctx context.Context, player *domain.Player) error
-	LinkPlayer(ctx context.Context, player *domain.Player, player2 *domain.Player) error
-	GetPlayerByName(ctx context.Context, name string) (error, []domain.Player)
-	UpdatePlayerName(ctx context.Context, player *domain.Player, newName string) error
-	DeletePlayer(ctx context.Context, player *domain.Player) error
+type DiscordPlayerStore interface {
+	NewDiscordPlayer(ctx context.Context, player *domain.DiscordPlayer) error
+	LinkDiscordPlayer(ctx context.Context, player *domain.DiscordPlayer, player2 *domain.WargamePlayer) error
+	GetDiscordPlayerByName(ctx context.Context, name string) ([]domain.DiscordPlayer, error)
+	UpdateDiscordPlayerName(ctx context.Context, player *domain.DiscordPlayer, newName string) error
+	DeleteDiscordPlayer(ctx context.Context, player *domain.DiscordPlayer) error
+}
+
+type WargamePlayerStore interface {
+	NewWargamePlayer(ctx context.Context, player *domain.WargamePlayer) error
+	LinkWargamePlayer(ctx context.Context, player *domain.WargamePlayer, player2 *domain.DiscordPlayer) error
+	GetWargamePlayerByName(ctx context.Context, name string) ([]domain.WargamePlayer, error)
+	UpdateWargamePlayerName(ctx context.Context, player *domain.WargamePlayer, newName string) error
+	DeleteWargamePlayer(ctx context.Context, player *domain.WargamePlayer) error
+	IsLinkedToDiscordPlayer(ctx context.Context, player *domain.WargamePlayer) (bool, error)
+	GetDiscordLink(ctx context.Context, player *domain.WargamePlayer) (*domain.DiscordPlayer, error)
 }
 
 type MapStore interface {
-	GetMapByName(ctx context.Context, name string) (error, []domain.Map)
-	GetMapByCode(ctx context.Context, code string) (error, []domain.Map)
-	GetMapBySize(ctx context.Context, size int) (error, []domain.Map)
+	// All maps that contain the substring "name"
+	GetMapByName(ctx context.Context, name string) ([]domain.Map, error)
+	//All maps that contain the substring code
+	GetMapByCode(ctx context.Context, code string) ([]domain.Map, error)
+	// All maps that are of Size size
+	GetMapBySize(ctx context.Context, size int) ([]domain.Map, error)
+	// All maps that are of Kind kind
+	GetMapByKind(ctx context.Context, size int) ([]domain.Map, error)
 }
 
 type ModeStore interface {
@@ -36,9 +52,9 @@ type ModeStore interface {
 }
 
 type IconStore interface {
-	GetIconById(ctx context.Context, id int) (error, *domain.Icon)
-	GetIconByName(ctx context.Context, name string) (error, *domain.Icon)
-	GetAllIcons(ctx context.Context) (error, []domain.Icon)
+	GetIconById(ctx context.Context, id int) (*domain.Icon, error)
+	GetIconByName(ctx context.Context, name string) (*domain.Icon, error)
+	GetAllIcons(ctx context.Context) ([]domain.Icon, error)
 }
 
 type CommandStore interface {
