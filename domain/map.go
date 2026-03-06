504400f5161515e144a4b9800e021c32788fc6f2
dolftenhave <dolftenhave@gmail.com>
2026-03-06 15:45:52 +1300

    Added Kind to Map

--- domain/map.go
+++ domain/map.go
@@ -2,8 +2,9 @@ package domain
 
 // Map represents a wargame map
 type Map struct {
-	Code  string
-	Name  string
-	Image string
-	Size  string
+	Code  string // The code of the map
+	Name  string // The name of the map
+	Image string // A link to the maps image
+	Kind  string // The maps kind. Land, Mixed, Naval.
+	Size  int	// The size of the map. 1: 1v1, 2: 2v2, 3: 3v3, 4: 4v4, 10: 10v10
 }
